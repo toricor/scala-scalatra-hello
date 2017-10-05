d0ae567ebb2e87655fad3729daec47aff6e9275c
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-05 22:20:51 +0900

    Sampleデータを削除

--- src/main/scala/com/toricor/demo/slickexample.scala
+++ src/main/scala/com/toricor/demo/slickexample.scala
@@ -1,7 +1,5 @@
 package com.toricor.demo
 
-import java.time.LocalDateTime
-
 import org.scalatra.{FutureSupport, ScalatraBase, ScalatraServlet}
 import slick.jdbc.H2Profile.api._
 
@@ -38,74 +36,25 @@ object Tables {
     def event_id = column[Int]("EVENT_ID")
     def * = (id, user_id, event_id)
 
-    def user = foreignKey("USER_RESERVE_FK", user_id, users)(_.id)
+    def user = foreignKey("USER_RESERVATION_FK", user_id, users)(_.id)
     def event = foreignKey("EVENT_FK", event_id, events)(_.id)
   }
 
-  // Definition of the SUPPLIERS table
-  class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-    def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-    def name = column[String]("SUP_NAME")
-    def street = column[String]("STREET")
-    def city = column[String]("CITY")
-    def state = column[String]("STATE")
-    def zip = column[String]("ZIP")
-
-    // Every table needs a * projection with the same type as the table's type parameter
-    def * = (id, name, street, city, state, zip)
-  }
-
-  // Definition of the COFFEES table
-  class Coffees(tag: Tag) extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
-    def name = column[String]("COF_NAME", O.PrimaryKey)
-    def supID = column[Int]("SUP_ID")
-    def price = column[Double]("PRICE")
-    def sales = column[Int]("SALES")
-    def total = column[Int]("TOTAL")
-    def * = (name, supID, price, sales, total)
-
-    // A reified foreign key relation that can be navigated to create a join
-    def supplier = foreignKey("SUP_FK", supID, suppliers)(_.id)
-  }
-
-  // Table query for the SUPPLIERS table, represents all tuples
-  val suppliers = TableQuery[Suppliers]
+  // Table query for the Ugit SERS table, represents all tuples
   val users = TableQuery[Users]
   val events = TableQuery[Events]
   val reservations = TableQuery[Reservations]
 
-  // Table query for the COFFEES table
-  val coffees = TableQuery[Coffees]
 
-  // Query, implicit inner join coffes and suppliers, return their names
-  val findCoffeesWithSuppliers = {
-    for {
-      c <- coffees
-      s <- c.supplier
-    } yield (c.name, s.name)
-  }
-
+  // Query, implicit inner join events and users, return their names
   val findEventsWithAuthors = {
     for {
       e <- events
-      u  <- e.user
+      u <- e.user
     } yield (e.title, u.name)
   }
 
   // DBIO Action which runs several queries inserting sample data
-  val insertSupplierAndCoffeeData = DBIO.seq(
-    Tables.suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199"),
-    Tables.suppliers += (49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460"),
-    Tables.suppliers += (150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966"),
-    Tables.coffees ++= Seq(
-      ("Colombian", 101, 7.99, 0, 0),
-      ("French_Roast", 49, 8.99, 0, 0),
-      ("Espresso", 150, 9.99, 0, 0),
-      ("Colombian_Decaf", 101, 8.99, 0, 0),
-      ("French_Roast_Decaf", 49, 9.99, 0, 0)
-    )
-  )
-
   val insertUsersEventsAndReservationData = DBIO.seq(
     Tables.users += (12, "hoge"),
     Tables.users += (14, "hage"),
@@ -141,7 +90,7 @@ trait SlickRoutes extends ScalatraBase with FutureSupport {
     db.run(Tables.dropSchemaAction)
   }
 
-  get("/coffees") {
+  get("/events/with-authors") {
     db.run(Tables.findEventsWithAuthors.result) map { xs =>
       println(xs)
       contentType = "text/plain"
