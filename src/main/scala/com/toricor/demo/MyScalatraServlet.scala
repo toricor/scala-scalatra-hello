7de46110eae16da339e1e0ccb0e2ff79cc135bac
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-05 10:42:12 +0900

    routerの実験

--- src/main/scala/com/toricor/demo/MyScalatraServlet.scala
+++ src/main/scala/com/toricor/demo/MyScalatraServlet.scala
@@ -3,13 +3,32 @@ package com.toricor.demo
 import org.scalatra._
 
 class MyScalatraServlet extends ScalatraServlet {
+  // routeのmatchチェックは下から読まれる
 
   get("/") {
     views.html.hello()
   }
 
-  get("/hello") {
+  // match only when you're 127.0.0.1(localhost)
+  get("/hello/:name", request.getRemoteHost == "127.0.0.1") {
+    <p>Hello, {params("name")}</p>
+  }
+
+
+  get("/hello/redirect") {
+    halt(status = 301, headers = Map("Location" -> "http://example.org/"))
+  }
+
+  get("/api/users") {
     "this message will be json"
+  }
+
+  get("/api/users/:id") {
+    halt(status = 301, headers = Map("Location" -> "http://example.org/"))
+  }
+
+  post("/api/users/:id") {
+    ""
   }
 
 }
