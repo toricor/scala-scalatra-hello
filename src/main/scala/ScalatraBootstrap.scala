ef3acf92a8d0e5f71f001f97129ec390bf26ee88
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-05 17:23:38 +0900

    scalatraのslickexampleに合わせた

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,35 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import org.slf4j.LoggerFactory
 import com.toricor.demo._
 import org.scalatra._
 import javax.servlet.ServletContext
 
+import slick.jdbc.H2Profile.api._
+
+/**
+  * This is the ScalatraBootstrap bootstrap file. You can use it to mount servlets or
+  * filters. It's also a good place to put initialization code which needs to
+  * run at application start (e.g. database configurations), and init params.
+  */
 class ScalatraBootstrap extends LifeCycle {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
+  val cpds = new ComboPooledDataSource
+  logger.info("Created c3p0 connection pool")
+
   override def init(context: ServletContext) {
-    context.mount(new MyScalatraServlet, "/*")
+    val db = Database.forDataSource(cpds, None)   // create the Database object
+    context.mount(new SlickApp(db), "/*")   // create and mount the Scalatra application
+  }
+
+  private def closeDbConnection() {
+    logger.info("Closing c3po connection pool")
+    cpds.close
+  }
+
+  override def destroy(context: ServletContext) {
+    super.destroy(context)
+    closeDbConnection
   }
 }
